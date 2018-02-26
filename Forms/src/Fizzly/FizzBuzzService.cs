1bf72ecdd60f6af619f66be46b8599e23cd467ec
Bryce Klinker <bklinker@pillartechnology.com>
2018-02-26 13:59:12 -0600

    Moved FizzBuzzService to its own file. Moved initial value to field initializer

---
+++ Forms/src/Fizzly/FizzBuzzService.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Fizzly.Shared.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Fizzly
+{
+    public class FizzBuzzService
+    {
+        private readonly IHttpClient _httpClient;
+
+        public FizzBuzzService(IHttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<FizzBuzz> GetLatest()
+        {
+            var response = await _httpClient.GetAsync("http://localhost:9000");
+            var json = await response.Content.ReadAsStringAsync();
+            var jObject = JsonConvert.DeserializeObject<JObject>(json);
+            return new FizzBuzz(jObject);
+        }
+    }
+}
--- Forms/src/Fizzly/MainViewModel.cs
+++ Forms/src/Fizzly/MainViewModel.cs
@@ -1,9 +1,6 @@
-﻿using System.Threading.Tasks;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 using Fizzly.Shared;
 using Fizzly.Shared.Http;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace Fizzly
 {
@@ -11,38 +8,19 @@ namespace Fizzly
     {
         private readonly FizzBuzzService _service;
 
-        public string Value { get; set; }
+        public string Value { get; set; } = "";
         public ICommand FizzBuzzCommand { get; }
 
         public MainViewModel(IHttpClient httpClient)
         {
             _service = new FizzBuzzService(httpClient);
             FizzBuzzCommand = new DelegateCommand(UpdateValue);
-            Value = "";
         }
 
         private async void UpdateValue()
         {
             var fizzBuzz = await _service.GetLatest();
             Value = fizzBuzz.Evalate();
-        }
-    }
-
-    public class FizzBuzzService
-    {
-        private readonly IHttpClient _httpClient;
-
-        public FizzBuzzService(IHttpClient httpClient)
-        {
-            _httpClient = httpClient;
-        }
-
-        public async Task<FizzBuzz> GetLatest()
-        {
-            var response = await _httpClient.GetAsync("http://localhost:9000");
-            var json = await response.Content.ReadAsStringAsync();
-            var jObject = JsonConvert.DeserializeObject<JObject>(json);
-            return new FizzBuzz(jObject);
         }
     }
 }
