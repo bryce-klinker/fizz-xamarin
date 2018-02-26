72313022378016f642e2dbf7f7de2fd965bdec94
Bryce Klinker <bklinker@pillartechnology.com>
2018-02-26 09:30:23 -0600

    Started adding forms project

---
+++ Forms/tests/Fizzly.UITests/Tests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace Fizzly.UITests
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        private IApp _app;
+        private readonly Platform _platform;
+
+        public Tests(Platform platform)
+        {
+            _platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            _app = AppInitializer.StartApp(_platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            _app.Screenshot("First screen.");
+        }
+    }
+}
+
